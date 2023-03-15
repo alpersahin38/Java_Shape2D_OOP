8568914c26779151d7d73cfadfa89f3e93f01f59
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-03-15 23:44:30 +0300

    added

---
+++ src/ce204_hw1_algo_lib/EllipticalShape.java
@@ -0,0 +1,14 @@
+package ce204_hw1_algo_lib;
+
+
+/**
+ * The EllipticalShape class is an abstract class that extends the Shape2D class and represents a 2-dimensional elliptical shape.
+ * 
+ * It contains a protected field for the semi-major axis of the elliptical shape.
+ */
+public abstract class EllipticalShape extends Shape2D{
+	
+	protected double semiMajorAxis;
+
+	
+}
